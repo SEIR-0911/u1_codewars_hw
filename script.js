dabad7b2277921c897276ab6cefe3f4852bb674d
Logan <logankrieger317@gmail.com>
2023-09-13 16:55:54 -0500

    Code Wars Homework initial Submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-## Return Negative
-
-```js
+// problem 1
 const  makeNegative = (num) => {
     return -Math.abs(num);
   }
@@ -8,34 +6,23 @@ const  makeNegative = (num) => {
   console.log(makeNegative(5)); 
   console.log(makeNegative(-5)); 
   console.log(makeNegative(0));  
-  
-```
 
-## Sum of Positive
-
-```js
-
+// problem 2
 const numbers = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15,100];
 let sum = 0;
 
 numbers.forEach((num) => { if(num > 0){ sum += num; }});
 
 console.log(sum); 
-```
 
-## Function 2
 
-```js
+// problem 3
 const sqReturn = (sqr) => {sqr = Math.sqrt(sqr) 
     console.log(sqr)};
 
 sqReturn(25);
 
-```
-
-## Sum Arrays
-
-```js
+// problem 4
 function sumOfNumbers(arr) {
     if (arr.length === 0) {
       return 0;
@@ -48,14 +35,11 @@ function sumOfNumbers(arr) {
   console.log(sumOfNumbers([-1, -2, -3, -4])); 
   console.log(sumOfNumbers([1.5, 2.5]));
   console.log(sumOfNumbers([])); 
-```
+  
 
-## Reversed Strings
-
-```js
+// problem 5
 const reverseString = (string) => {
     console.log(string.split("").reverse().join(""));
   }
 
  reverseString("some string of text here");
-```
