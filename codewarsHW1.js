c8febc043bf39b51d63a26f775c2230eb4673055
sammymarks <sammy.marks@gmail.com>
2023-09-14 06:36:39 -0500

    Sammy Marks CodeWarsHW1 Submission #1

--- codewarsHW1.js
+++ codewarsHW1.js
@@ -1,6 +1,4 @@
-## Return Negative
-
-```js
+//Q 1
 
 // arrow function to call the number
 // conditional ternary to take the negative of the absolute value of the number except if it's zero
@@ -9,16 +7,13 @@ const makeNegative = (num) => {
     (num == 0) ? console.log(0) : console.log(-Math.abs(num))
 }
 
-makeNegative(1);    // return -1
-makeNegative(-5);   // return -5
-makeNegative(0);    // return 0
-makeNegative(0.12); // return -0.12
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
 
-```
+// Q2 Sum Positive
 
-## Sum of Positive
-
-```js
 
 const sumOfPositive = (array) => {
     // remove negatives with .filter
@@ -30,30 +25,20 @@ const sumOfPositive = (array) => {
     console.log(output)
 }
 
-sumOfPositive([1,2,3,-1,-2,-3])
-sumOfPositive([0, -1])
-sumOfPositive([1,-4,7,12])
+// sumOfPositive([1,2,3,-1,-2,-3])
+// sumOfPositive([0, -1])
+// sumOfPositive([1,-4,7,12])
 
-```
+// Q3 Squaring
 
-## Function 2
-
-```js
-
-//Use Math.pow for power-of
 const square = (num) => console.log(Math.pow(num,2))
 
-square(2)
-square(-2)
-square(1.32)
-//why does this last one have a trailing 2 after the correct answer? -> 1.7424000000000002
+// square(2)
+// square(-2)
+// square(1.32)
 
 
-```
-
-## Sum Arrays
-
-```js
+//Q4 sum arrays
 
 //simplified from Q2 answer...
 
@@ -65,16 +50,15 @@ const sumOfArray = (array) => {
     console.log(output)
 }
 
-sumOfArray([1, 5.2, 4, 0, -1])
-sumOfArray([])
-sumOfArray([-2.398])
+// sumOfArray([1, 5.2, 4, 0, -1])
+// sumOfArray([])
+// sumOfArray([-2.398])
 
 
-```
+//Q5 Reverse String
 
-## Reversed Strings
 
-```js
+
 const stringReverse = wordInput => {
     //Define an empty output string
     let wordOutput = new String()
@@ -88,4 +72,3 @@ const stringReverse = wordInput => {
 stringReverse(`World`)
 stringReverse(`Never eat soggy waffles`)
 stringReverse(``) //What does this answer indicate? [String: '']
-```
