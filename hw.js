bc637356cda50585093f843dee9431960e499eb7
Rhys Frazier <rhysrfrazier@gmail.com>
2023-09-13 20:28:12 -0400

    created hw file to test and added answers

---
+++ hw.js
@@ -0,0 +1,42 @@
+//testing my answeres in the console here instead of in the codewars console, in case I accidentally submit something there when I just want to test
+//1______________________________________________
+
+// function makeNegative(num){
+//     if(num < 0){
+//         return num;
+//     } else if (num > 0){
+//         return 0-num
+//     } else {
+//         return 0
+//     }
+// }
+// console.log(makeNegative(2))
+// console.log(makeNegative(5))
+// console.log(makeNegative(-3))
+// console.log(makeNegative(0))
+
+//2____________________________
+// const arr = [1, -4, 7, 12]
+
+// const positiveSum = arr.filter((num) => {
+//    return num>0
+// }).reduce((ac, val) => {
+//     return ac+val
+// })
+// console.log(positiveSum)
+
+//3________________________
+// const squareMe = (num) => Math.pow(num, 2)
+// console.log(squareMe(3))
+
+//4_______________________
+// const arr = [1, -4, 7, 12]
+// const sumArray = arr.reduce((ac, val) => {
+//     return ac + val
+// }, 0)
+// console.log(sumArray)
+
+//5_________________________
+// const arr = [1, -4, 7, 12]
+// const reverseMe = arr.reverse()
+// console.log(reverseMe)
--- solution.md
+++ solution.md
@@ -1,29 +1,46 @@
 ## Return Negative
 
 ```js
-
+function makeNegative(num){
+    if(num < 0){
+        return num;
+    } else if (num > 0){
+        return 0-num
+    } else {
+        return 0
+    }
+}
 ```
 
 ## Sum of Positive
 
 ```js
-
+//const arr = [1, -4, 7, 12]
+const positiveSum = arr.filter((num) => {
+   return num>0
+}).reduce((ac, val) => {
+    return ac+val
+})
 ```
 
 ## Function 2
 
 ```js
-
+const squareMe = (num) => Math.pow(num, 2)
 ```
 
 ## Sum Arrays
 
 ```js
-
+//const arr = [1, -4, 7, 12]
+const sumArray = arr.reduce((ac, val) => {
+    return ac + val
+}, 0)
 ```
 
 ## Reversed Strings
 
 ```js
-
+const arr = [1, -4, 7, 12]
+const reverseMe = arr.reverse()
 ```
