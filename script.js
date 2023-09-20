773936ea02b589fba5de068900e5e63dfb44038b
Kevin <kevinbarber0@outlook.com>
2023-09-20 11:07:20 -0400

    hw

--- script.js
+++ script.js
@@ -53,7 +53,7 @@
 
             function solution(str){
             let reversed = ''
-            for(let i = str.length - 1; i >= 0; i-){
+            for(let i = str.length - 1; i >= 0; i--){
             reversed += str[i]
             }
             return reversed}
